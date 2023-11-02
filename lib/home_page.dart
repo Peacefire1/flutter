661a2b37bd2c9dc95284750e6f6c0c0b454bc8f6
Peacefire1 <ganzorigenkhtsogt@gmail.com>
2023-11-02 11:51:33 +0800

    about BoxDecoration

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,23 +4,54 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ColoredBox(
-        color: Colors.red,
+      body: Container(
+        padding: EdgeInsets.all(100),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.red],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            image: DecorationImage(image: NetworkImage("")),
+            shape: BoxShape.circle,
+            color: Colors.amberAccent,
+            // borderRadius: BorderRadius.all(Radius.circular(50)),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 20,
+                offset: Offset(3, 6),
+                spreadRadius: -8,
+                color: Color.fromRGBO(0, 0, 0, .4),
+              )
+            ]),
         child: Text(
           "This is String",
           style: TextStyle(
-            backgroundColor: Colors.amberAccent,
-            color: Colors.blueGrey,
-            decoration: TextDecoration.lineThrough,
             fontSize: 30,
             fontWeight: FontWeight.bold,
-            letterSpacing: 1,
-            wordSpacing: 4,
           ),
           textAlign: TextAlign.center,
         ),
       ),
     );
+    // return Scaffold(
+    //   body: ColoredBox(
+    //     color: Colors.red,
+    //     child: Text(
+    //       "This is String",
+    //       style: TextStyle(
+    //         backgroundColor: Colors.amberAccent,
+    //         color: Colors.blueGrey,
+    //         decoration: TextDecoration.lineThrough,
+    //         fontSize: 30,
+    //         fontWeight: FontWeight.bold,
+    //         letterSpacing: 1,
+    //         wordSpacing: 4,
+    //       ),
+    //       textAlign: TextAlign.center,
+    //     ),
+    //   ),
+    // );
     // return Scaffold(
     //   body: Padding(
     //     padding: EdgeInsets.only(top: 100, left: 100),
