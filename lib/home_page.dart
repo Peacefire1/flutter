fd65928cfee6169c2a9a97daefcd0f0385fb22f3
Peacefire1 <ganzorigenkhtsogt@gmail.com>
2023-11-02 10:19:24 +0800

    StatefulWidget and StatelessWidget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   String title = "Initial";
+
+  void setTitile() {
+    setState(() {
+      title = "Switched";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(title),
-      ],
-    );
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          MyText(title),
+          ElevatedButton(onPressed: setTitile, child: Text("Switch"))
+        ],
+      ),
+    ));
+  }
+}
+
+class MyText extends StatelessWidget {
+  String title;
+  MyText(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(title);
   }
 }
