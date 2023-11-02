61debaf9e0a6acadb7b80447c68c20b43651b5bb
Peacefire1 <ganzorigenkhtsogt@gmail.com>
2023-11-02 11:27:44 +0800

    Center and Padding, ColoredBox
    git add .

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,15 +4,57 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: ColoredBox(
+        color: Colors.red,
         child: Text(
           "This is String",
           style: TextStyle(
-              backgroundColor: Colors.amberAccent,
-              color: Colors.blueGrey,
-              decoration: TextDecoration.lineThrough),
+            backgroundColor: Colors.amberAccent,
+            color: Colors.blueGrey,
+            decoration: TextDecoration.lineThrough,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1,
+            wordSpacing: 4,
+          ),
+          textAlign: TextAlign.center,
         ),
       ),
     );
+    // return Scaffold(
+    //   body: Padding(
+    //     padding: EdgeInsets.only(top: 100, left: 100),
+    //     // padding: EdgeInsets.all(100),
+    //     child: Text(
+    //       "This is String",
+    //       style: TextStyle(
+    //         backgroundColor: Colors.amberAccent,
+    //         color: Colors.blueGrey,
+    //         decoration: TextDecoration.lineThrough,
+    //         fontSize: 30,
+    //         fontWeight: FontWeight.bold,
+    //         letterSpacing: 1,
+    //         wordSpacing: 4,
+    //       ),
+    //     ),
+    //   ),
+    // );
+    // return Scaffold(
+    //   body: Center(
+    //     heightFactor: 10.5,
+    //     child: Text(
+    //       "This is String",
+    //       style: TextStyle(
+    //         backgroundColor: Colors.amberAccent,
+    //         color: Colors.blueGrey,
+    //         decoration: TextDecoration.lineThrough,
+    //         fontSize: 30,
+    //         fontWeight: FontWeight.bold,
+    //         letterSpacing: 1,
+    //         wordSpacing: 4,
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
